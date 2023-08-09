7f86e0fea9be35a6432137301b4769443dc09624
Malcolm Hyson <malcolm@serverx.net>
2023-08-09 00:10:20 -0400

    updating comments

--- app.py
+++ app.py
@@ -4,20 +4,29 @@ from flask import Flask
 # declare the flask app
 app = Flask(__name__)
 
-# sample calculation
+
 @app.route("/calc/<int:v1>/<int:v2>")
 def calc(v1, v2):
-    result  = v1 + v2
+    """
+    sample calculation
+    """
+    result = v1 + v2
     return f"<p>{v1} + {v2} = {result}</p>"
 
-# display the current datetime
+
 @app.route("/")
 def default_route():
+    """
+    display the current datetime
+    """
     now = datetime.now()
     return f"<p>Current datetime {now}</p>"
 
-# start the flask app
-if __name__=="__main__":
+
+if __name__ == "__main__":
+    """
+    start the flask app
+    """
     # default flask port
     port = 5000
     print(f"starting app on port: {port}")
--- readme.md
+++ readme.md
@@ -4,4 +4,4 @@
 `docker build . -t py_docker`
 
 ## Running the container
-`docker run --rm -it -p 80:80 --name pyd py_docker `
+`docker run --rm -it -p 5000:5000 --name pyd py_docker`
